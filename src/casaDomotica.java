132e0a7df0bff4fc655b9b4c55704cac55ea33b9
Anastasia <alu.anastasia.gubareva@mataro.epiaedu.cat>
2025-10-20 16:38:22 +0200

    Primer commit

---
+++ src/casaDomotica.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class casaDomotica {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    // Variables  globals
+    private static double temperature = 21.0;
+
+    public static void main(String[] args) {
+        boolean program = false;
+
+        while (!program) {
+            
+        }
+
+    }
+}
